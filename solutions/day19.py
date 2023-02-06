784916342ede30bb6ac004d7e0051eac4a56cc91
ryan-heslin <rwheslin@gmail.com>
2023-02-06 18:50:50 -0500

    Victory at any price

--- solutions/day19.py
+++ solutions/day19.py
@@ -1,3 +1,6 @@
+import re
+
+
 def parse(lines):
     result = {}
 
@@ -16,11 +19,6 @@ def parse(lines):
 
 
 def resolve(string, rules):
-    end = len(string)
-    if False and len(rules[8]) > 1 and string == "babbbbaabbbbbabbbbbbaabaaabaaa":
-        breakpoint()
-
-    print(string)
 
     # Problem: decide how many times to invoke recursive option of recursive rules
     def inner(rule, index):
@@ -28,12 +26,7 @@ def resolve(string, rules):
         for alternative in specs:
             i = index
             for token in alternative:
-                if i > end - 1:
-                    # breakpoint()
-                    return i
                 if type(token) is str:
-                    print(i)
-                    print(token == string[i])
                     i = -1 if token != string[i] else i + 1
                 else:
                     # Recursive
@@ -45,6 +38,49 @@ def resolve(string, rules):
         return -1
 
     return inner(0, 0) == len(string)
+
+
+def wrap(string):
+    return f"(?:{string})"
+
+
+def to_regex(rule, variable_rules):
+    specs = rules[rule]
+    first = specs[0]
+
+    if type(first[0]) == str:
+        return first[0]
+    result = "".join(to_regex(new_rule, variable_rules) for new_rule in first)
+    if len(result) > 1:
+        result = wrap(result)
+    if len(specs) > 1 and rule not in variable_rules:
+        second = "".join(to_regex(new_rule, variable_rules) for new_rule in specs[1])
+        if len(second) > 1:
+            second = wrap(second)
+        result = wrap(
+            "|".join(
+                [
+                    result,
+                    second,
+                ]
+            )
+        )
+    return result
+
+
+def verify(string, left_regex, right_regex, full_regex):
+    if re.match(full_regex, string):
+        left = re.match("^" + left_regex + "+", string)
+        left_matches = re.findall(left_regex, string[: left.end()])
+        right_matches = re.findall(right_regex, string[left.end() :])
+        # assert re.match(right_regex + "+$", string[left.end() :])
+        # assert "".join(left_matches) + "".join(right_matches) == string
+        return (
+            "".join(left_matches) + "".join(right_matches) == string
+            and len(right_matches) > 0
+            and len(left_matches) > len(right_matches)
+        )
+    return False
 
 
 with open("inputs/day19.txt") as f:
@@ -60,7 +96,14 @@ print(part1)
 new_rules = """8: 42 | 42 8
 11: 42 31 | 42 11 31"""
 new_rules_parsed = parse(new_rules.splitlines())
-rules.update(new_rules_parsed)
+variable_rules = tuple(new_rules_parsed.keys())
 
-part2 = sum(list(resolve(string, rules) for string in strings))
+left_regex = wrap(to_regex(rules[variable_rules[0]][0][0], variable_rules))
+right_regex = wrap(to_regex(rules[variable_rules[1]][0][1], variable_rules))
+if rules[0] == reversed(variable_rules):
+    left_regex, right_regex = right_regex, left_regex
+
+full_regex = "^" + left_regex + "+" + right_regex + "+$"
+
+part2 = sum(verify(string, left_regex, right_regex, full_regex) for string in strings)
 print(part2)
